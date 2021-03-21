47691fc150c80ac700604b3f0a037e274bcb98da
Alexandre Fernandes Junior <alejr.dev@gmail.com>
2021-03-21 20:46:25 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Temperature log
+A really, really simple temperature logging project to be used on some iot applications
---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+
+const PORT = 5000;
+const TEMPS_FILE = "temps.csv";
+const TEMPS_FILE_SEPARATOR = ",";
+const app = express();
+
+app.use(cors());
+
+app.post("/temperatures/:temp", (req, resp, _next) => {
+  const { temp } = req.params;
+  console.log("received temp", temp);
+  const now = new Date();
+  const line = `${now.toISOString()}${TEMPS_FILE_SEPARATOR}${temp}\n`;
+  fs.appendFile(TEMPS_FILE, line, (err) => {
+    if (err)
+      return resp
+        .status(500)
+        .send({ error: "could not save temperature record" });
+    resp.sendStatus(201);
+  });
+});
+
+//This endpoint is called when sonoff button is pressed
+app.post("/buttons/:sonoffId", (req, resp, _next) => {
+  const { sonoffId } = req.params;
+  resp.sendStatus(204);
+});
+
+app.get("/temperatures", (_req, resp, _next) => {
+  fs.readFile(TEMPS_FILE, (err, data) => {
+    if (err) return resp.status(500).json({ error: "database error" });
+    const temperatures = data
+      .toString()
+      .split("\n")
+      .filter((line) => !!line)
+      .map((line) => {
+        const [date, temp] = line.split(TEMPS_FILE_SEPARATOR);
+        return {
+          date,
+          temp: +temp,
+        };
+      });
+    resp.json({ temperatures });
+  });
+});
+
+fs.stat(TEMPS_FILE, (err) => {
+  if (err) {
+    fs.writeFile(TEMPS_FILE, "", (err) => {
+      if (err) return console.log("Cannot create temperature file");
+      console.log("Temperature file created");
+      startApi();
+    });
+  } else {
+    console.log("Temperature file already created");
+    startApi();
+  }
+});
+
+const startApi = () => {
+  app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+  });
+};
